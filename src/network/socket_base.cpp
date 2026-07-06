68939d5ffabf38de99778ad2a4725101f717d3c3
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-05 23:32:35 -0400

    socketbase abstract class

---
+++ src/network/socket_base.cpp
@@ -0,0 +1,40 @@
+#include "network/socket_base.hpp"
+#include <iostream>
+
+SocketBase::SocketBase(
+    int domain, 
+    int service, 
+    int protocol, 
+    int port,
+    u_long interface
+) {
+    // Define adress (sockaddr_in)
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(interface);
+
+    // Define established connection
+    socket_fd = socket(domain, service, protocol);
+    test_connection(socket_fd);
+    connection = connect_to_network(socket_fd, address);
+    test_connection(connection);
+}
+
+void SocketBase::test_connection(int item) {
+    if (item < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Getters
+
+int SocketBase::get_socket_fd() {
+    return socket_fd;
+}
+int SocketBase::get_connection() {
+    return connection;
+}
+sockaddr_in SocketBase::get_adress() {
+    return address;
+}
