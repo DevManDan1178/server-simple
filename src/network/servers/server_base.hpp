d017af1309d9bc5db2a327a6e85ce37803aa75a3
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-11 00:28:16 -0400

    + abstracted with boost and asio

--- src/network/servers/server_base.hpp
+++ src/network/servers/server_base.hpp
@@ -1,29 +1,62 @@
 #pragma once
 
-#include "network/sockets/sockets.hpp"
-#include <string>
+#include "logger.hpp"
+#include "data_structures/thread_safe_queue.hpp"
+
+#include <thread>
 #include <string_view>
-#include <netinet/in.h>
-#include <unistd.h>
-
-class ServerBase {
-protected:
-    constexpr static int buffer_size = 4096; //2^12
-    char buffer[buffer_size] = {0};
-    
-    ListeningSocket* const socket;
-    int backlog;
+#include <boost/asio.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <iostream>
+#include <string>
+#include <utility>
 
 
-    
-    virtual std::string process_client_request(int client, const sockaddr_in& socket_address, socklen_t address_size, const std::string_view request) = 0;
-    virtual void respond(int socket_fd, std::string_view response);
+class server_base {
+    protected:
+        boost::asio::io_context asio_context;
+    private:
+        boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard;
+    protected:    
+        boost::asio::ip::tcp::acceptor asio_acceptor;
+        
 
+    public:
+        server_base(unsigned short port) : 
+            asio_context(),
+            work_guard(
+                boost::asio::make_work_guard(asio_context)
+            ),
+            asio_acceptor(
+                asio_context, 
+                boost::asio::ip::tcp::endpoint(
+                    boost::asio::ip::tcp::v4(),
+                    port
+                )
+            ) {
+                
+        }
 
-public:
-    ServerBase(int domain, int service, int protocol, int port, u_long interface, int backlog);
-    
-    virtual void launch() = 0;
+        virtual ~server_base() {
+            stop();
+        };
 
-    ListeningSocket* const get_socket();
+        virtual void launch() {
+            log_debug() << "[SERVER] launching \n";
+
+            start_accept_async();
+            asio_context.run(); 
+        }
+
+        virtual void stop() {
+            asio_context.stop();
+
+            log_debug() << "[SERVER] stopped \n"; 
+        }
+
+    protected:
+        virtual void start_accept_async() = 0;
+        
+        virtual void handle_client(std::shared_ptr<boost::asio::ip::tcp::socket> socket) = 0;
 };
