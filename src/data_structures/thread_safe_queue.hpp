083ccbae37b3397656ff36d781f5ca5bbbf81178
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-12 01:30:14 -0400

    + error when queue is empty and calling for front/back, fixed thread safe methods

--- src/data_structures/thread_safe_queue.hpp
+++ src/data_structures/thread_safe_queue.hpp
@@ -3,103 +3,152 @@
 #include <condition_variable>
 #include <mutex>
 #include <deque>
+#include <stdexcept>
+#include <utility>
 
 template<typename T>
 class thread_safe_queue {
-    protected:
-        std::mutex mutex_queue;
-        std::deque<T> dequeue;
+protected:
+    std::mutex mutex_queue;
+    std::condition_variable waiting;
+    std::deque<T> dequeue;
+    bool stopped = false;
 
+public:
+    thread_safe_queue() = default;
 
-        std::mutex mutex_waiting;
-        std::condition_variable waiting;
+    thread_safe_queue(const thread_safe_queue<T>&) = delete;
 
+    virtual ~thread_safe_queue() {
+        stop();
+    }
 
-    public:
-        thread_safe_queue() = default;
-        thread_safe_queue(const thread_safe_queue<T>&) = delete;
-        virtual ~thread_safe_queue() {
-            clear();
+    T front() {
+        std::scoped_lock lock(mutex_queue);
+
+        if (dequeue.empty()) {
+            throw std::runtime_error("Queue is empty");
         }
 
-        const T& front() {
-            std::scoped_lock lock (mutex_queue);
-            return dequeue.front();
+        return dequeue.front();
+    }
+
+    T back() {
+        std::scoped_lock lock(mutex_queue);
+
+        if (dequeue.empty()) {
+            throw std::runtime_error("Queue is empty");
         }
 
-        const T& back() {
-            std::scoped_lock lock(mutex_queue);
-            return dequeue.back();
-        }
+        return dequeue.back();
+    }
 
-        void push_back(const T& item) {
+    void push_back(const T& item) {
+        {
             std::scoped_lock lock(mutex_queue);
             dequeue.emplace_back(item);
-            stop_wait();
         }
 
-        void push_back(T&& item) {
+        waiting.notify_one();
+    }
+
+    void push_back(T&& item) {
+        {
             std::scoped_lock lock(mutex_queue);
-            dequeue.emplace_back(item);
-            stop_wait();
+            dequeue.emplace_back(std::move(item));
         }
 
+        waiting.notify_one();
+    }
 
-        void push_front(const T& item) {
-            std::scoped_lock lock (mutex_queue);
-            return dequeue.emplace_front(item);
-            stop_wait();
-        }
-
-        
-        void push_front(T&& item) {
-            std::scoped_lock lock (mutex_queue);
-            return dequeue.emplace_front(std::move(item));
-            stop_wait();
+    void push_front(const T& item) {
+        {
+            std::scoped_lock lock(mutex_queue);
+            dequeue.emplace_front(item);
         }
 
-        bool empty() {
-            std::scoped_lock lock (mutex_queue);
-            return dequeue.empty();
+        waiting.notify_one();
+    }
+
+    void push_front(T&& item) {
+        {
+            std::scoped_lock lock(mutex_queue);
+            dequeue.emplace_front(std::move(item));
         }
 
-        size_t size() {
-            std::scoped_lock lock (mutex_queue);
-            return dequeue.size();
+        waiting.notify_one();
+    }
+
+    bool empty() {
+        std::scoped_lock lock(mutex_queue);
+        return dequeue.empty();
+    }
+
+    size_t size() {
+        std::scoped_lock lock(mutex_queue);
+        return dequeue.size();
+    }
+
+    void clear() {
+        std::scoped_lock lock(mutex_queue);
+        dequeue.clear();
+    }
+
+    T pop_front() {
+        std::scoped_lock lock(mutex_queue);
+
+        if (dequeue.empty()) {
+            throw std::runtime_error("Queue is empty");
         }
 
-        void clear() {
-            std::scoped_lock lock (mutex_queue);
-            dequeue.clear();  
+        T item = std::move(dequeue.front());
+        dequeue.pop_front();
+
+        return item;
+    }
+
+    T pop_back() {
+        std::scoped_lock lock(mutex_queue);
+
+        if (dequeue.empty()) {
+            throw std::runtime_error("Queue is empty");
         }
 
+        T item = std::move(dequeue.back());
+        dequeue.pop_back();
 
-        T pop_front() {
-            std::scoped_lock lock (mutex_queue);
-            T t = std::move(dequeue.front());
-            dequeue.pop_front();
-            return t;
-        }
-        
-        T pop_back() {
-             std::scoped_lock lock (mutex_queue);
-            T t = std::move(dequeue.back());
-            dequeue.pop_back();
-            return t;
+        return item;
+    }
+
+    /**
+     * Blocks until an item is available or the queue is stopped.
+     */
+    T wait_and_pop() {
+        std::unique_lock<std::mutex> lock(mutex_queue);
+
+        waiting.wait(lock, [this] {
+            return stopped || !dequeue.empty();
+        });
+
+        if (stopped && dequeue.empty()) {
+            throw std::runtime_error("Queue stopped");
         }
 
-        /**
-         * @brief 
-         */
-        void wait() {
-            while(empty()) {
-                std::unique_lock<std::mutex> unique_lock(mutex_waiting);
-                waiting.wait(unique_lock);
-            }
+        T item = std::move(dequeue.front());
+        dequeue.pop_front();
+
+        return item;
+    }
+
+    /**
+     * @brief Wakes up all waiting threads and prevents further waiting.
+     */
+    void stop() {
+        {
+            std::scoped_lock lock(mutex_queue);
+            stopped = true;
         }
-    private:
-        void stop_wait() {
-            std::unique_lock<std::mutex> unique_lock(mutex_waiting);
-            waiting.notify_one();
-        }
+
+        waiting.notify_all();
+    }
 };
