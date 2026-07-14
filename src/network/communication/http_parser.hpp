2eff1df37d56b9d0b0db93834f2472a66602537d
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-14 01:21:48 -0400

    + namespace for parsing http query

---
+++ src/network/communication/http_parser.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+namespace http_parser {
+    json parse_query(const std::string& target) {
+        json params = json::object();
+
+        auto pos = target.find('?');
+        if (pos == std::string::npos) {
+            return params;
+        }
+
+        std::string query = target.substr(pos + 1);
+
+        size_t start = 0;
+        while (start < query.size()) {
+            size_t amp = query.find('&', start);
+            if (amp == std::string::npos) {
+                amp = query.size();
+            }
+
+            std::string pair = query.substr(start, amp - start);
+
+            size_t eq = pair.find('=');
+            if (eq != std::string::npos) {
+                std::string key = pair.substr(0, eq);
+                std::string value = pair.substr(eq + 1);
+
+                // Parsing value in order (float -> int -> default to string)
+                try {
+                    size_t pos;
+                    long long i = std::stoll(value, &pos);
+
+                    // Entire string was an integer
+                    if (pos == value.size()) {
+                        params[key] = i;
+                    } else {
+                        throw std::invalid_argument("not int");
+                    }
+                }
+                catch (...) {
+                    try {
+                        size_t pos;
+                        double f = std::stod(value, &pos);
+
+                        // Entire string was a float
+                        if (pos == value.size()) {
+                            params[key] = f;
+                        } else {
+                            params[key] = value;
+                        }
+                    }
+                    catch (...) {
+                        params[key] = value;
+                    }
+                }
+            }
+
+            start = amp + 1;
+        }
+
+        return params;
+    }
+}
