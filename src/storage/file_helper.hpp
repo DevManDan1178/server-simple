c97e501302917d28afcf9099a58d9e29ef12324b
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-14 18:26:37 -0400

    + getting file path overload without subdirectory

--- src/storage/file_helper.hpp
+++ src/storage/file_helper.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "logger.hpp"
 #include <string_view>
 #include <filesystem>
 namespace file_helper {
@@ -27,7 +28,7 @@ namespace file_helper {
      * @param filename File name.
      * @return Full path to the JSON file.
      */
-    inline static std::string get_file_path(const std::filesystem::path& parent_directory, const std::string& subdirectory_name, std::string_view filename) {
+    inline static std::string get_file_path(const std::filesystem::path& parent_directory, std::string_view subdirectory_name, std::string_view filename) {
         namespace fs = std::filesystem;
         try {
             if (!is_valid_filename(filename)) {
@@ -44,7 +45,37 @@ namespace file_helper {
             
             return (directory / file).string();
         } catch (const fs::filesystem_error& e) {
-            throw std::runtime_error("Unable to create " + subdirectory_name + " directory for filename: " + std::string(filename) + " - " + std::string(e.what()));
+            throw std::runtime_error("Unable to create " + std::string(subdirectory_name) + " directory for filename: " + std::string(filename) +
+            + "under " + std::string(parent_directory) + "/" + std::string(subdirectory_name) +
+            " - " + std::string(e.what()));
+        }
+    }
+
+    /**
+     * @brief Creates the storage path for a file.
+     * @param parent_directory Base storage directory.
+     * @param filename File name.
+     * @return Full path to the JSON file.
+     */
+    inline static std::string get_file_path(const std::filesystem::path& parent_directory, std::string_view filename) {
+        namespace fs = std::filesystem;
+        try {
+            if (!is_valid_filename(filename)) {
+                throw std::invalid_argument("Invalid filename - file_helper");
+            }
+
+            fs::create_directories(parent_directory);     
+            fs::path file(filename);
+
+            if (file.extension() != ".json") {
+                file += ".json";
+            } 
+            
+            return (parent_directory / file).string();
+        } catch (const fs::filesystem_error& e) {
+            throw std::runtime_error("Unable to create directory for filename: " + std::string(filename) + 
+                "under " + std::string(parent_directory) + 
+                " - " + std::string(e.what()));
         }
     }
 }
