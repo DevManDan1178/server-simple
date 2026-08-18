9e8c8f6722cd8a267bbb0a2f9464231cee8fa439
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-17 23:26:04 -0400

    + broken pipe treated as a disconnect

--- src/network/communication/websocket_connection.hpp
+++ src/network/communication/websocket_connection.hpp
@@ -108,6 +108,7 @@ class websocket_session : public std::enable_shared_from_this<websocket_session>
                     if (ec) {
                         if (ec != boost::asio::error::operation_aborted &&
                             ec != boost::beast::websocket::error::closed &&
+                            ec != boost::asio::error::broken_pipe &&
                             ec != boost::asio::error::connection_reset) {
                             std::cerr << "[web_socket] Non-disconnect write error: " << ec.message() << "\n";
                         }                
